ee3163ec5dfd811f68d9584cfc6c5d471d5779c2
J. Jefferson <criacao.jefferson@gmail.com>
2021-01-07 17:19:41 -0300

    add protection against sqlinjection and post verif

--- public/php/login.php
+++ public/php/login.php
@@ -1,8 +1,15 @@
 <?php
 include('conexao.php');
 
-$user = $_POST['user'];
-$pass = $_POST['pass'];
+#verificação se veio dado via POST;
+if(empty($_POST['user']) || empty($_POST['pass'])) {
+    header('Location: ../index.php');
+    exit();
+}
+
+#proteção contra SQLinjection;
+$user = mysqli_real_escape_string($conexao, $_POST['user']);
+$pass = mysqli_real_escape_string($conexao, $_POST['pass']);
 
 $query = "select * from `user` where nome='$user' and senha='$pass'";
 
@@ -11,6 +18,8 @@ $row = mysqli_num_rows($result);
 
 if($row == 1):
     header('Location: painel.php');
+    exit()
 else:
     header('Location: ../index.php');
+    exit()
 endif;
