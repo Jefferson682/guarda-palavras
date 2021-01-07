491e7eff42361280a41cd2a2afff3d4c5df87f9a
J. Jefferson <criacao.jefferson@gmail.com>
2021-01-07 17:04:38 -0300

    add sql

--- docker/mysql/estrutura.sql
+++ docker/mysql/estrutura.sql
@@ -1,1 +1,17 @@
-#aqui vc coloca a estrutura das suas tabelas.
+#anotações para bando de dados.
+use ambiente;
+
+create table `user` (
+id int auto_increment not null,
+nome varchar(50) not null,
+senha char(32),
+primary key (id)
+);
+
+insert into `user` (nome, senha) values ('Joana','1471'),('Jefferson','123');
+
+select * from `user`;
+
+
+#para adicionar da query do php
+select * from `user` where nome='Jefferson' and senha='123';
